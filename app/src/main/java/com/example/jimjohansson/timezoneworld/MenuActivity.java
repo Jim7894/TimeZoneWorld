ac8730fe73db7dc89e5e22a633e1d5ba5cd3f3f0
Xares01 <jimvaldemar@gmail.com>
2018-04-04 18:22:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jimjohansson/timezoneworld/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.jimjohansson.timezoneworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
+//implementation 'com.android.support:support:customtabs:27.0.2'
